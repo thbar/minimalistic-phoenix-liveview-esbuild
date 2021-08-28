7e88260601787b7d21e6b61b93edae4c4abdccee
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-28 17:37:54 +0200

    Add a simple self-refreshing LiveView

--- as_mix_install.exs
+++ as_mix_install.exs
@@ -13,6 +13,33 @@ Mix.install([
 ])
 
 Application.put_env(:my_app, MyApp.Endpoint, server: true, http: [port: 8081])
+defmodule SimpleWeb.ClockLive do
+  use Phoenix.LiveView
+  use Phoenix.HTML
+  import Phoenix.LiveView.Helpers
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <p>It's <%= @date %></p>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+
+    {:ok, put_date(socket)}
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, put_date(socket)}
+  end
+
+  defp put_date(socket) do
+    assign(socket, date: NaiveDateTime.local_now())
+  end
+end
 
 defmodule MyApp.Router do
   use Phoenix.Router
