759c14a278b6e99ce5766596dda1164a2a57d737
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-28 16:08:27 +0200

    Reduce boilerplate further

--- lib/simple_web/views/layout_view.ex
+++ lib/simple_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule SimpleWeb.LayoutView do
-  use SimpleWeb, :view
+  use Phoenix.View,
+    root: "lib/simple_web/templates",
+    namespace: SimpleWeb
+
+  # Use all HTML functionality (forms, tags, etc)
+  use Phoenix.HTML
+
+  alias SimpleWeb.Router.Helpers, as: Routes
 end
