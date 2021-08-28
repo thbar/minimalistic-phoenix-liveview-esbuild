354e05e9f65694e0a213a76719445b867e119827
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-28 15:42:16 +0200

    Remove assets

--- lib/simple_web/endpoint.ex
+++ lib/simple_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule SimpleWeb.Endpoint do
     at: "/",
     from: :simple,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
