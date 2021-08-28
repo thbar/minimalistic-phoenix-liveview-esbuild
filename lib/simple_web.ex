685ecbb5a23846e692dcdc06db97b91f86393dc4
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-28 15:49:42 +0200

    Remove unused parts

--- lib/simple_web.ex
+++ lib/simple_web.ex
@@ -1,31 +1,4 @@
 defmodule SimpleWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use SimpleWeb, :controller
-      use SimpleWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
-  def controller do
-    quote do
-      use Phoenix.Controller, namespace: SimpleWeb
-
-      import Plug.Conn
-      alias SimpleWeb.Router.Helpers, as: Routes
-    end
-  end
-
   def view do
     quote do
       use Phoenix.View,
