4367b38d73416d34e881e5a42ff47b66fbd54b56
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-28 15:58:02 +0200

    Remove more plugs

--- lib/simple_web/endpoint.ex
+++ lib/simple_web/endpoint.ex
@@ -30,16 +30,6 @@ defmodule SimpleWeb.Endpoint do
     plug Phoenix.CodeReloader
   end
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
-
-  plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
-
-  plug Plug.MethodOverride
-  plug Plug.Head
   plug Plug.Session, @session_options
   plug SimpleWeb.Router
 end
