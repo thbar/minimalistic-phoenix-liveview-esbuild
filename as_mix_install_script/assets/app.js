6771fc70dd7c30423705db40333c009f1c9255e6
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-08-29 18:29:14 +0200

    Trim down things a bit

--- as_mix_install_script/assets/app.js
+++ as_mix_install_script/assets/app.js
@@ -1,14 +1,7 @@
-// We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "./app.css"
-
-// Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
-
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
-
 liveSocket.connect()
-
 window.liveSocket = liveSocket
